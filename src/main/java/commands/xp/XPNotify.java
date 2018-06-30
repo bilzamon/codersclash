a7236f3f195366a03f7755c2b05e8d5237632ede
Hove Gotthard <hovegotthard@web.de>
2018-06-30 14:23:06 +0200

    what changed here

--- src/main/java/commands/xp/XPNotify.java
+++ src/main/java/commands/xp/XPNotify.java
@@ -20,17 +20,21 @@ public class XPNotify extends CommandHandler {
 		super("xpnotify");
 	}
 
-	/* (non-Javadoc)
-	 * @see command.CommandHandler#execute(command.CommandManager.ParsedCommandString, net.dv8tion.jda.core.events.message.MessageReceivedEvent)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * command.CommandHandler#execute(command.CommandManager.ParsedCommandString,
+	 * net.dv8tion.jda.core.events.message.MessageReceivedEvent)
 	 */
 	@Override
 	public void execute(ParsedCommandString parsedCommand, MessageReceivedEvent event) {
 		UserData data = UserData.fromId(event.getAuthor().getId());
-		
-		if(data.getLvlupNotify()) {
+
+		if (data.getLvlupNotify()) {
 			data.setLvlupNotify(false);
 			event.getTextChannel().sendMessage("LevelUp message successful disabled").queue();
-		}else {
+		} else {
 			data.setLvlupNotify(true);
 			event.getTextChannel().sendMessage("LevelUp message successful enabled").queue();
 		}
