6d5567bcd02459d502ce7c896dd6e37a61fb73b9
Hove Gotthard <hovegotthard@web.de>
2018-06-30 14:14:11 +0200

    added mention and username search

--- src/main/java/commands/xp/XP.java
+++ src/main/java/commands/xp/XP.java
@@ -17,22 +17,35 @@ public class XP extends CommandHandler {
 
 	@Override
 	public void execute(ParsedCommandString parsedCommand, MessageReceivedEvent event) {
+		String id = "";
 		if (parsedCommand.getArgs().length == 0) {
 			UserData data = UserData.fromId(event.getAuthor().getId());
 			if (data != null) {
 				event.getChannel()
 						.sendMessage(new EmbedBuilder().setColor(Color.green)
-								.setTitle("Level: " + data.getLevel() + " (" + Level.remainingXp(data.getTotalXp()) + "/"
-										+ Level.xpToLevelUp(data.getLevel()) + ")" + "XP")
+								.setTitle("Level: " + data.getLevel() + " (" + Level.remainingXp(data.getTotalXp())
+										+ "/" + Level.xpToLevelUp(data.getLevel()) + ")" + "XP")
 								.setFooter(event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator(),
 										event.getAuthor().getAvatarUrl())
 								.build())
 						.queue();
-			} else {
-				event.getTextChannel().sendMessage("no xp").queue();
 			}
-		} else {
-			// TODO other users
+		} else if (event.getMessage().getMentionedMembers().size() > 0) {
+			id = event.getMessage().getMentionedMembers().get(0).getUser().getId();
+		} else if (event.getGuild().getMembersByEffectiveName(parsedCommand.getArgs()[0], true).size() > 0) {
+			id = event.getGuild().getMembersByEffectiveName(parsedCommand.getArgs()[0], true).get(0).getUser().getId();
+		}
+		UserData data = UserData.fromId(id);
+		if (data != null) {
+			event.getChannel()
+					.sendMessage(new EmbedBuilder().setColor(Color.green)
+							.setTitle("Level: " + data.getLevel() + " (" + Level.remainingXp(data.getTotalXp()) + "/"
+									+ Level.xpToLevelUp(data.getLevel()) + ")" + "XP")
+							.setFooter(event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator(),
+									event.getAuthor().getAvatarUrl())
+							.build())
+					.queue();
+
 		}
 	}
 }
