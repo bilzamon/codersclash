4a06a1a19f3a35f0218c8667c26e42c832c5976c
Hove Gotthard <hovegotthard@web.de>
2018-07-01 22:00:36 +0200

    added Listener

--- src/main/java/commands/VierGewinnt.java
+++ src/main/java/commands/VierGewinnt.java
@@ -11,56 +11,70 @@ import util.Settings;
 
 public class VierGewinnt extends CommandHandler {
 
-    private JDA jda = Main.getJda();
+	private JDA jda = Main.getJda();
 
-    public VierGewinnt() {super("vg"); }
+	public VierGewinnt() {
+		super("vg", "vg", "");
+		//TODO add parameter usage
+	}
 
-    @Override
-    public void execute(CommandManager.ParsedCommandString parsedCommand, MessageReceivedEvent event) {
-        String[] args = parsedCommand.getArgs();
-        int heigh = Integer.parseInt(args[0]);
-        int width = Integer.parseInt(args[1]);
+	@Override
+	public void execute(CommandManager.ParsedCommandString parsedCommand, MessageReceivedEvent event) {
+		String[] args = parsedCommand.getArgs();
+		int heigh = Integer.parseInt(args[0]);
+		int width = Integer.parseInt(args[1]);
 
-        User challenger = event.getAuthor();
-        String opponent = args[2];
+		User challenger = event.getAuthor();
+		String opponent = args[2];
 
+		if (args.length < 3) {
 
-        if(args.length < 3) {
+			event.getTextChannel().sendMessage(error.setDescription("Bitte benutze den Befehl so: " + Settings.PREFIX
+					+ "vg <Höhe(7-8)> <Breite(Um 1 kleiner als Höhe)> <Gegenspieler>").build()).queue();
+		}
 
-            event.getTextChannel().sendMessage(error.setDescription("Bitte benutze den Befehl so: " + Settings.PREFIX + "vg <Höhe(7-8)> <Breite(Um 1 kleiner als Höhe)> <Gegenspieler>").build()).queue();
-        }
+		if (event.getGuild().getMembersByName(opponent, true).get(0).getOnlineStatus() != OnlineStatus.ONLINE) {
 
-        if(event.getGuild().getMembersByName(opponent, true).get(0).getOnlineStatus() != OnlineStatus.ONLINE) {
+			event.getTextChannel()
+					.sendMessage(error
+							.setDescription(
+									"Der User " + opponent + " konnte nicht gefunden werden oder ist nicht online!")
+							.build())
+					.queue();
+		}
 
-            event.getTextChannel().sendMessage(error.setDescription("Der User " + opponent + " konnte nicht gefunden werden oder ist nicht online!").build()).queue();
-        }
+		if (heigh > 6 && heigh < 9) {
+			if (width == heigh - 1) {
+				if (opponent == challenger.getName()) {
+					try {
 
-        if(heigh > 6 && heigh < 9) {
-            if(width == heigh - 1) {
-                if(opponent == challenger.getName()) {
-                    try {
+						jda.getUsersByName(opponent, true).get(0).openPrivateChannel().queue(privateChannel -> {
+							privateChannel.sendMessage(challenger.getName() + " hat dich zu einer Runde Vier-Gewinnt("
+									+ heigh + "x" + width + ") herausgefordert!").queue();
+						});
 
-                        jda.getUsersByName(opponent, true).get(0).openPrivateChannel().queue(privateChannel -> {
-                            privateChannel.sendMessage(challenger.getName() + " hat dich zu einer Runde Vier-Gewinnt(" + heigh + "x" + width + ") herausgefordert!").queue();
-                        });
+					} catch (Exception exeception) {
 
-                    } catch(Exception exeception) {
+						exeception.printStackTrace();
+					}
+				} else {
 
-                        exeception.printStackTrace();
-                    }
-                } else {
+					event.getTextChannel()
+							.sendMessage(error.setDescription("Du kannst dich nicht selbst herausfordern!").build())
+							.queue();
+				}
 
-                    event.getTextChannel().sendMessage(error.setDescription("Du kannst dich nicht selbst herausfordern!").build()).queue();
-                }
+			} else {
 
-            } else {
+				event.getTextChannel()
+						.sendMessage(error.setDescription("Die Breite muss um 1 kleiner sein als die Höhe!").build())
+						.queue();
+			}
 
-                event.getTextChannel().sendMessage(error.setDescription("Die Breite muss um 1 kleiner sein als die Höhe!").build()).queue();
-            }
+		} else {
 
-        } else {
-
-            event.getTextChannel().sendMessage(error.setDescription("Die Höhe muss zwischen 7 und 8 liegen!").build()).queue();
-        }
-    }
+			event.getTextChannel().sendMessage(error.setDescription("Die Höhe muss zwischen 7 und 8 liegen!").build())
+					.queue();
+		}
+	}
 }
--- src/main/java/listeners/VierGewinntListener.java
+++ src/main/java/listeners/VierGewinntListener.java
@@ -4,16 +4,23 @@ import net.dv8tion.jda.core.events.message.guild.react.GuildMessageReactionAddEv
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class VierGewinntListener extends ListenerAdapter {
+	
+	public void onGuildMessageReactionAdd(GuildMessageReactionAddEvent event) {
+		System.out.println(readReaction(event));
+	}
+	
+	
+	public static int readReaction(GuildMessageReactionAddEvent guildMessageReactionAddEvent) {
+		String emoteName = guildMessageReactionAddEvent.getReactionEmote().getName();
+		System.out.println("test");
+		switch (emoteName) {
+		case "✅":
+			return 1;
 
-    public static int readReaction(GuildMessageReactionAddEvent guildMessageReactionAddEvent) {
-        String emoteName = guildMessageReactionAddEvent.getReactionEmote().getName();
+		case "❌":
+			return 2;
+		}
 
-        switch (emoteName) {
-            case "✅": return 1;
-
-            case "❌": return 2;
-        }
-
-        return 0;
-    }
+		return 0;
+	}
 }
