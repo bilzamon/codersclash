6b26db7b03a5387b04049f89c1e8b4903315131f
UnknownNobodyYT <unknownnbody@web.de>
2018-07-07 11:39:51 +0200

    Added the startGame-function to start the building of the game

--- src/main/java/listeners/VierGewinntListener.java
+++ src/main/java/listeners/VierGewinntListener.java
@@ -1,22 +1,47 @@
 package listeners;
 
+import core.Main;
+import db.GameData;
 import db.MySQL;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.priv.react.PrivateMessageReactionAddEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.awt.*;
 
 public class VierGewinntListener extends ListenerAdapter {
 
 	@Override
 	public void onPrivateMessageReactionAdd(PrivateMessageReactionAddEvent privateMessageReactionAddEvent) {
-		if(MySQL.getGameData(privateMessageReactionAddEvent.getMessageId()) != null){
+		if(MySQL.getGameData(privateMessageReactionAddEvent.getMessageId()) != null) {
 			if(!privateMessageReactionAddEvent.getUser().isBot()) {
+				GameData gameData = MySQL.getGameData(privateMessageReactionAddEvent.getMessageId());
+				String emoteName = privateMessageReactionAddEvent.getReactionEmote().getName();
 
-				privateMessageReactionAddEvent.getChannel().sendMessage("Gültig").queue();
+				Main.getJda().getPrivateChannelById(privateMessageReactionAddEvent.getChannel().getId())
+						.deleteMessageById(privateMessageReactionAddEvent.getReaction().getMessageId()).queue();
 
-			} else {
+				switch (emoteName) {
 
-				privateMessageReactionAddEvent.getChannel().sendMessage("Ungültig").queue();
+					case "✅":
+
+						privateMessageReactionAddEvent.getChannel().sendMessage(new EmbedBuilder().setColor(Color.DARK_GRAY)
+								.setDescription("Spiel wird vorbereitet...").build())
+								.complete();
+
+						startGame(gameData);
+						break;
+
+					case "❌":
+
+						privateMessageReactionAddEvent.getChannel().sendMessage(new EmbedBuilder().setColor(Color.HSBtoRGB(85, 1, 100))
+								.setDescription("Spielanfrage abgelehnt!").build())
+								.complete();
+						break;
+				}
 			}
 		}
 	}
+
+	private static void startGame(GameData gameData) {}
 }
